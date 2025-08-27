de87e1714b82e7cab597b3fe1a7b879ff283310f
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2025-08-27 10:19:17 +0200

    change args

--- cmd/collection.go
+++ cmd/collection.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -48,8 +47,8 @@ func init() {
 var collectionCmd = &cobra.Command{
 	Use:   "collection",
 	Short: "Download collection albums",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
+	Args:  cobra.ExactArgs(0),
+	RunE: func(cmd *cobra.Command, args []string) error {
 		var cookies string
 		if cookieEnv := os.Getenv("BANDCAMP_COOKIES"); cookieEnv != "" {
 			cookies = cookieEnv
@@ -68,9 +67,10 @@ var collectionCmd = &cobra.Command{
 
 		err := collectionDownloader.Download(username, cookies)
 		if err != nil {
-			fmt.Println(err)
+			return err
 		}
 
 		log.Println("Done")
+		return nil
 	},
 }
